571f678dba84eaeeefc37368cb6754104ac30db1
floppyist <floppyism@gmail.com>
2025-02-24 09:19:56 +0100

    Initial commit

---
+++ src/main/java/org/guengeln/floc/FlocApplication.java
@@ -0,0 +1,13 @@
+package org.guengeln.floc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/guengeln/floc/FlocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.guengeln.floc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
