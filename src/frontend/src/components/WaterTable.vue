66b887b25d080cc68adfcb1289fce17003f23bc2
floppyist <floppyism@gmail.com>
2025-03-18 10:04:05 +0100

    Implemented/refactored client data fetch by using v-for function from vuejs

--- src/frontend/src/components/WaterTable.vue
+++ src/frontend/src/components/WaterTable.vue
@@ -1,91 +1,10 @@
 <script setup>
-import axios from 'axios';
-import { onBeforeMount } from 'vue';
-
-onBeforeMount(() => {
-    axios.get('/api/clients/getAll')
-        .then(res => {
-            var table = document.getElementById("tblElectricity").getElementsByTagName('tbody')[0];
-
-            if (res.data.length == 0) {
-                table.hidden = true;
-            } else {
-                table.hidden = false;
-
-                for (var i = 1; i < res.data.length; i++) {
-                    var newRow = table.insertRow();
-                    var newCell;
-                    var newText;
-                    var newElement;
-
-                    console.log(res.data[i].id);
-
-                    newCell = newRow.insertCell(0);
-                    newText = document.createTextNode(res.data[i].active);
-                    newCell.appendChild(newText);
-
-                    newCell = newRow.insertCell(1);
-                    newText = document.createTextNode(res.data[i].parcel);
-                    newCell.appendChild(newText);
-
-                    newCell = newRow.insertCell(2);
-                    newText = document.createTextNode(res.data[i].name);
-                    newCell.appendChild(newText);
-
-                    newCell = newRow.insertCell(3);
-                    newText = document.createTextNode(res.data[i].address);
-                    newCell.appendChild(newText);
-
-                    newCell = newRow.insertCell(4);
-                    newText = document.createTextNode(res.data[i].phone);
-                    newCell.appendChild(newText);
-
-                    newCell = newRow.insertCell(5);
-                    newText = document.createTextNode(res.data[i].waterlevel_old);
-                    newCell.appendChild(newText);
-
-                    newCell = newRow.insertCell(6);
-                    newText = document.createTextNode(res.data[i].waterlevel_new);
-                    newCell.appendChild(newText);
-
-                    newCell = newRow.insertCell(7);
-                    newText = document.createTextNode('');
-                    newCell.appendChild(newText);
-
-                    newCell = newRow.insertCell(8);
-                    newElement = document.createElement('button');
-                    newElement.setAttribute('type', 'button');
-                    newElement.setAttribute('class', 'btn btn-success');
-                    newElement.innerHTML = 'Bearbeiten';
-                    newCell.appendChild(newElement);
-
-                    newElement = document.createElement('span');
-                    newCell.appendChild(newElement);
-
-                    newElement = document.createElement('button');
-                    newElement.setAttribute('type', 'button');
-                    newElement.setAttribute('class', 'btn btn-danger');
-                    newElement.innerHTML = 'Löschen';
-                    newCell.appendChild(newElement);
-
-                    console.log(`${i}`);
-                }
-            }
-        });
-});
-
-function deleteClient(id) {
-    console.log('Client with ID ' + id + ' will be deleted.');
-
-
-    axios.delete('/api/clients/delete/' + id)
-        .then((response) => {
-            console.log(response);
-        })
-        .catch((error) => {
-            console.log(error);
-        })
-}
+defineProps({
+    items: {
+        type: Array,
+        required: true
+    }
+})
 </script>
 
 <template>
@@ -103,6 +22,16 @@ function deleteClient(id) {
             </tr>
         </thead>
         <tbody>
+            <tr v-for="item in items" :key="item.id">
+                <td>{{ item.active }}</td>
+                <td>{{ item.parcel }}</td>
+                <td>{{ item.name }}</td>
+                <td>{{ item.address }}</td>
+                <td>{{ item.phone }}</td>
+                <td>{{ item.waterlevel_old }}</td>
+                <td>{{ item.waterlevel_new }}</td>
+                <td>PLACEHOLDER</td>
+            </tr>
         </tbody>
     </table>
     <div class="col text-center">
--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { onMounted, ref } from 'vue';
 import WaterTable from '../components/WaterTable.vue'
+
+const items = ref([]);
+
+onMounted(async () => {
+    const response = await fetch('http://localhost:8080/api/clients/getAll');
+
+    items.value = await response.json();
+});
 </script>
 
 <template>
     <main>
-        <WaterTable />
+        <WaterTable :items="items" />
     </main>
 </template>
